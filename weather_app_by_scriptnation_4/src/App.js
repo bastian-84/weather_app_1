cc747da25ff4f30b7271a820775596a0000159a0
Sebastian Reichenbach <sebastianreichenbach@Mac-mini-clean-1014.local>
2021-03-23 12:02:58 -0700

    seb, timestamp 06.4 - 44:00

--- weather_app_by_scriptnation_4/src/App.js
+++ weather_app_by_scriptnation_4/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import DisplayWeather from  './components/DisplayWeather.js';
+// import axios
+import Axios from 'axios';
 
 
 class App extends React.Component {
@@ -9,6 +12,9 @@ class App extends React.Component {
     coords: {
       latitude: 0,
       longitude: 0
+    },
+    data: {
+
     }
   }
 
@@ -27,6 +33,28 @@ class App extends React.Component {
 
         // state wird umgeschrieben
         this.setState({coords: newCoords});
+
+        // API call, axios
+        // API key "Test vorab aus 06.1"
+        // Axios.get(`http://api.weatherstack.com/current?access_key=02ba8c2b874b2834032f8e2e734ad677&query=berlin`);
+        // Axios Abfrage, API, eigener API key, long lat von state
+        Axios.get(`http://api.weatherstack.com/current?access_key=02ba8c2b874b2834032f8e2e734ad677&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(paramRes => {
+          console.log(paramRes);
+
+          let weatherData = {
+            sLocation: paramRes.data.location.name,
+            sTemperature: paramRes.data.current.temperature, 
+            sDescription: paramRes.data.current.weather_descriptions[0],
+            sRegion: paramRes.data.location.region, 
+            sCountry: paramRes.data.location.country,
+            sWind_speed: paramRes.data.current.wind_speed,
+            sPressure: paramRes.data.current.pressure,
+            sPrecip: paramRes.data.current.precip,
+            sHumidity: paramRes.data.current.humidity, 
+            sImg: paramRes.data.current.weather_icons
+          }
+          this.setState({ data: weatherData });
+        });
       })
     }else{
       console.log(`html5 geolocation is not supported`);
@@ -36,6 +64,7 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
+        <DisplayWeather />
         
       </div>
     );
